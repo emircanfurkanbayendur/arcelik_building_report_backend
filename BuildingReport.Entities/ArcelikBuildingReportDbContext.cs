7fc34f464aeac761453fb1401778379f5b8c8cd1
Emircan Furkan Bayendur <emircanfurkanbayendur@gmail.com>
2023-03-13 14:01:56 +0300

    Connection String moved to appsettings.json file

--- BuildingReport.Entities/ArcelikBuildingReportDbContext.cs
+++ BuildingReport.Entities/ArcelikBuildingReportDbContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using BuildingReport.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace BuildingReport.DataAcess;
 
@@ -29,8 +30,20 @@ public partial class ArcelikBuildingReportDbContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=arcelik_building_report_db3;Trusted_Connection=true;TrustServerCertificate=True;");
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("MyDbContext"));
+        }
+    }
+
+    //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+    //        => optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=arcelik_building_report_db3;Trusted_Connection=true;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
