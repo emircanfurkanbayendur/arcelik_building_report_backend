cb530748bc95373cfef9704541adf644fb3e0c93
Emircan Furkan Bayendur <emircanfurkanbayendur@gmail.com>
2023-05-02 15:45:00 +0300

    configurations for Azure Deployment

--- BuildingReport.API/Program.cs
+++ BuildingReport.API/Program.cs
@@ -148,13 +148,14 @@ app.UseMiddleware<ExceptionMiddleware>();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
-
+app.UseSwagger();
+app.UseSwaggerUI();
 //app.ConfigureExceptionHandler(logger);
 //app.ConfigureCustomExceptionMiddleware();
 
